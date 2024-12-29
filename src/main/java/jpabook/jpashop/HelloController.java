74eba4d535337c73b0ccbe7dce1c358d907ee207
chanho <cse05091@gmail.com>
2024-12-29 13:12:29 +0900

    커밋테스트

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+    // 커밋테스트
     @GetMapping("hello")
     public String Hello(Model model){
         model.addAttribute("data", "hello!!!");
